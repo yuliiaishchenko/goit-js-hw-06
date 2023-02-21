94073188fffe747f0337d9de3a8d21856d11c979
yuliiaishchenko <romashka.ukr@gmail.com>
2023-02-21 13:44:48 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,13 @@
-const liItem = document.querySelectorAll('.item');
-console.log(liItem);
+const liItem = document.querySelectorAll('li.item');
+
+
+const numberOfAllCategories = liItem.length;
+console.log(`Number of categories: ${numberOfAllCategories}`);
+
+const categoryIt = liItem.forEach (el =>{
+    const categoryName = el.firstElementChild.textContent;
+    const numberOfElement = el.lastElementChild.children.length;
+
+    console.log(`Category: ${categoryName}
+    Elements: ${numberOfElement}`)
+})
