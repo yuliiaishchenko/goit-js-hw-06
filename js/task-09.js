e2bc7ea7530b9b5147000b984b4e67b09c381574
yuliiaishchenko <romashka.ukr@gmail.com>
2023-02-23 21:16:00 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const buttonChangeColor = document.querySelector('button.change-color');
+const spanColor = document.querySelector('span.color');
+
+
+buttonChangeColor.addEventListener('click', () => 
+{let color = getRandomHexColor();
+body.style.backgroundColor = color;
+spanColor.textContent = color;})
