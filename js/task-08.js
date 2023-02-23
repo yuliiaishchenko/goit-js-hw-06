20901fc9ecaf4054091ae44eb4414d6f06314479
yuliiaishchenko <romashka.ukr@gmail.com>
2023-02-23 21:06:48 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', submitOn);
 
+function submitOn (event){
+event.preventDefault()
+const{
+    elements: { email, password},
+}= event.currentTarget
+
+if (email.value === '' || password.value ===''){
+    return alert ('Заповніть, будь ласка, всі поля!')
+}
+const newUser = {email: email.value, password: password.value}
+console.log(newUser);
+event.currentTarget.reset()
+};
