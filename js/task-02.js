152c67c70d03b592f8a64ac8e24f25df977c69b4
yuliiaishchenko <romashka.ukr@gmail.com>
2023-02-24 15:28:16 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,38 +7,20 @@ const ingredients = [
   'Condiments',
 ];
 
-const navFirstItemEl = document.createElement('li');
-navFirstItemEl.classList.add('item');
-navFirstItemEl.textContent = 'Potatoes';
 
-const navSecondItemEl = document.createElement('li');
-navSecondItemEl.classList.add('item');
-navSecondItemEl.textContent = 'Mushrooms';
-
-const navThirdItemEl = document.createElement('li');
-navThirdItemEl.classList.add('item');
-navThirdItemEl.textContent = 'Garlic';
-
-const navFourthItemEl = document.createElement('li');
-navFourthItemEl.classList.add('item');
-navFourthItemEl.textContent = 'Tomatos';
-
-const navFifthItemEl = document.createElement('li');
-navFifthItemEl.classList.add('item');
-navFifthItemEl.textContent = 'Herbs';
-
-const navSixthItemEl = document.createElement('li');
-navSixthItemEl.classList.add('item');
-navSixthItemEl.textContent = 'Condiments';
 
 const navEl = document.getElementById('ingredients');
-navEl.append(navFirstItemEl, navSecondItemEl, navThirdItemEl, navFourthItemEl, navFifthItemEl, navSixthItemEl);
+const newArray = [];
+
+ingredients.forEach(ingredient => {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.className = 'item';
+  ingredientsItem.textContent = ingredient;
+  newArray.push(ingredientsItem)
+});
+navEl.append(...newArray);
+
 
   console.log(navEl)
 
-// console.log(navFirstItemEl)
-// console.log(navSecondItemEl)
-// console.log(navThirdItemEl)
-// console.log(navFourthItemEl)
-// console.log(navFifthItemEl)
-// console.log(navSixthItemEl)
+
