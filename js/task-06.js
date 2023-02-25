c34a3b0ff1c3674f4eb6a05ecc965e2cf51e4833
yuliiaishchenko <romashka.ukr@gmail.com>
2023-02-25 19:12:06 +0100

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,25 @@
-const onInput = document.getElementById("validation-input")
-onInput.addEventListener("blur", onInputBlur)
-function onInputBlur(value){
-    
-        onInput.classList.toggle("valid",onInput.value.length === 6)
-    ;
+// const inputEl = document.getElementById("validation-input");
 
-    onInput.classList.toggle("invalid", onInput.value.length > 6);
+// inputEl.addEventListener("blur", handleInputBlur);
+
+// function handleInputBlur(value){
+  
+// inputEl.dataset.length === inputEl.value.length ? inputEl.classList.toggle("valid"):
+// inputEl.classList.toggle("invalid");
+
+// }
+
+const onInput = document.getElementById("validation-input");
+const inputValue = onInput.getAttribute('data-length');
+
+onInput.addEventListener("blur", onInputBlur)
+function onInputBlur(){
+    onInput.classList.remove('valid','invalid')
+  if( Number(inputValue) === onInput.value.length)
+  { 
+    onInput.classList.toggle("valid");
+     }
+   else{
+ 
+  onInput.classList.toggle("invalid");}
 }
